0a3e96092c55d8f7957d11b0c0e1d349b7bb270c
mohamedalaa <mohamedalaakey33@gmail.com>
2022-10-13 02:01:03 +0200

    task3

---
+++ MainPage.jsx
@@ -0,0 +1,9 @@
+// import { View,Text } from "react-native";
+ 
+//  export const MainPage=()=>{
+//     return (
+//       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//         <Text>MainPage</Text>
+//       </View>
+//     );
+//   }
